a120a090f43f404939cc7ef8b58d28d8ed485e3c
Yuri Dias <yuri.dazevedo@gmail.com>
2021-06-15 09:40:59 -0300

    feat: create person class

---
+++ src/main/java/Pessoa.java
@@ -0,0 +1,12 @@
+public class Pessoa {
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Pessoa setNome(String nome) {
+        this.nome = nome;
+        return this;
+    }
+}
